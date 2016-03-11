07f92ad418233930b128719f283969757f502ede
Kirk North <kirk.w.north@gmail.com>
2016-03-11 17:39:39 -0500

    DEP: qc module
    The qc module has been deprecated and is no longer required. Some of its
    submodules, like the texture submodule, has been moved to the util
    module.

--- grid/qc/rebuild_texture.sh
+++
@@ -1,5 +0,0 @@
-# Remove and rebuild _compute_texture.so
-
-rm -f compute_texture.so
-f2py -m compute_texture -h _compute_texture.pyf src/compute_texture.f90 --overwrite-signature
-f2py --fcompiler=gfortran -c _compute_texture.pyf src/compute_texture.f90 
